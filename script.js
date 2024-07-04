d13e29b9b314f53f4c4423967099c150441e39db
Felipe Peruceli <105284817+SoyBrother@users.noreply.github.com>
2024-07-04 09:32:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPeerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    { //primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+        alternativas: ["alternativa1", "alternativa2"]
+    },
+    { //segundo objeto da lista de perguntas
+        enunciado: "pergunta2",
+        alternativa: ["alternativa1", "alternativa2"]
+
+    }
+]
